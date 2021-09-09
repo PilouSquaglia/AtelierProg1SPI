95120474d4c1b6e0ca2b61f99f15fa6bf1a285bf
PilouSquaglia <pilou.squaglia@laposte.net>
2021-09-09 13:54:58 +0200

    Update Atelier_3.py

--- Atelier_3.py
+++ Atelier_3.py
@@ -5,3 +5,49 @@ Created on Thu Sep  9 11:39:31 2021
 @author: Pilou
 """
 
+def somme1(L:list) -> int:
+    
+    """Admet en paramètre une liste
+d'entiers L et retourne la somme des valeurs de la liste"""
+    somme=0
+    
+    for i in L:
+        somme+=i
+        
+    return somme
+
+def somme2(L:list) -> int:
+    
+    """Admet en paramètre une liste
+d'entiers L et retourne la somme des valeurs de la liste"""
+    somme=0
+    
+    for i in range(len(L)):
+        somme+=L[i]
+        
+    return somme
+
+def somme3(L:list) -> int:
+    
+    """Admet en paramètre une liste
+d'entiers L et retourne la somme des valeurs de la liste"""
+    i=0
+    somme=0
+    while i<len(L):
+        somme+=L[i]
+        i+=1
+    return somme
+    
+
+
+def test_exercice1 ():
+    print("TEST SOMME")
+    #test liste vide
+    print("Test liste vide : ", somme3([]))
+    #test somme 11111
+    S=[1,10,100, 1000,10000]
+    print("Test somme 11111 : ", somme3(S))
+    
+    
+    
+test_exercice1()
