bf622f6667a58b6888451d48c82bbde7d4270c47
PilouSquaglia <pilou.squaglia@laposte.net>
2021-09-13 14:05:06 +0200

    update

--- Test_Atelier_3.py
+++ Test_Atelier_3.py
@@ -11,10 +11,39 @@ def test_separer():
     
     """Teste la fonction separer()"""
     lst=[]
-    print("Liste vide  : {}".format(separer(lst)))
+    attendu=[]
+    res=separer(lst)
+    test=attendu==res
+    print("Test avec lst=[]".format(lst))
+    print("Liste vide, Attendu {} : \nRésultat :{}".format(attendu,res))
+    print(test)
     lst=[4, 8, -2,-3,-1, 0, -7, 0, 6, 5 ]
-    print("Attendu [-2, -3, -1, -7, 0, 0, 4, 8, 6, 5]  \nRésultat : {}".format(separer(lst)))
+    attendu=[-2, -3, -1, -7, 0, 0, 4, 8, 6, 5]
+    res=separer(lst)
+    test=attendu==res
+    print("Test avec lst={}".format(lst))
+    print("Attendu {}  \nRésultat : {}".format(attendu, res))
+    print(test)
     lst=[4, 5, 6, 0]
-    print("Attendu [0, 4, 5, 6] \nRésultat : {}".format(separer(lst)))
+    attendu=[0, 4, 5, 6]
+    res=separer(lst)
+    test=attendu==res
+    print("Test avec lst={}".format(lst))
+    print("Attendu {} \nRésultat : {}".format(attendu,res))
+    print(test)
     lst=[-4, -5, -6, 0]
-    print("Attendu [-4, -5, -6, 0] \nRésultat : {}".format(separer(lst)))
+    attendu=[-4, -5, -6, 0]
+    res=separer(lst)
+    test=attendu==res
+    print("Test avec lst={}".format(lst))
+    print("Attendu {} \nRésultat : {}".format(attendu, res))
+    print(test)
+    lst=[0, 1, 2, 3]
+    attendu=[0, 1, 2, 3]
+    res=separer(lst)
+    test=attendu==res
+    print("Test avec lst={}".format(lst))
+    print("Attendu {} \nRésultat : {}".format(attendu,res))
+    print(test)
+    
+test_separer()
