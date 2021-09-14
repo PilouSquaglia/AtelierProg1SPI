3d4926768303f2716f3ccd40adb309a069f6cd11
PilouSquaglia <pilou.squaglia@laposte.net>
2021-09-14 09:06:35 +0200

    update

--- Test_Atelier_3.py
+++ Test_Atelier_3.py
@@ -10,54 +10,52 @@ from Atelier_3 import separer
 def test_separer():
     
     """Teste la fonction separer()"""
+    
     lst=[]
     attendu=[]
-    res=separer(lst)
-    test1=attendu==res
+    test1=attendu==separer(lst)
+    res="######ECHEC######"
     
     print("Test avec lst={}".format(lst))
-    print("Liste vide, Attendu {} : \nRésultat :{}".format(attendu,res))
+    print("Liste vide, Attendu {} : \nRésultat :{}".format(attendu,separer(lst)))
     print(test1)
     print("#############")
     
     lst=[4, 8, -2,-3,-1, 0, -7, 0, 6, 5 ]
     attendu=[-2, -3, -1, -7, 0, 0, 4, 8, 6, 5]
-    res=separer(lst)
-    test2=attendu==res
+    test2=attendu==separer(lst)
     
     print("Test avec lst={}".format(lst))
-    print("Attendu {}  \nRésultat : {}".format(attendu, res))
+    print("Attendu {}  \nRésultat : {}".format(attendu, separer(lst)))
     print(test2)
     print("#############")
     
     lst=[4, 5, 6, 0]
     attendu=[0, 4, 5, 6]
-    res=separer(lst)
-    test3=attendu==res
+    test3=attendu==separer(lst)
     
     print("Test avec lst={}".format(lst))
-    print("Attendu {} \nRésultat : {}".format(attendu,res))
+    print("Attendu {} \nRésultat : {}".format(attendu, separer(lst)))
     print(test3)
     print("#############")
     
     lst=[-4, -5, -6, 0]
     attendu=[-4, -5, -6, 0]
-    res=separer(lst)
-    test4=attendu==res
+    test4=attendu==separer(lst)
     print("Test avec lst={}".format(lst))
-    print("Attendu {} \nRésultat : {}".format(attendu, res))
+    print("Attendu {} \nRésultat : {}".format(attendu, separer(lst)))
     print(test4)
     print("#############")
     
     lst=[0, 1, 2, 3]
     attendu=[0, 1, 2, 3]
-    res=separer(lst)
-    test5=attendu==res
+    test5=attendu==separer(lst)
     
     print("Test avec lst={}".format(lst))
-    print("Attendu {} \nRésultat : {}".format(attendu,res))
+    print("Attendu {} \nRésultat : {}".format(attendu, separer(lst)))
     print(test5)
-    if test1 and test2 and test3 and test4 and test5:
-        print("######SUCCES######")
     
+    if test1 and test2 and test3 and test4 and test5:
+        res=print("######SUCCES######")
+    return res
 test_separer()
