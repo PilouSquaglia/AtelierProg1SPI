2a27367a000f672b75ecd6ffb801cd3ebe231dd5
PilouSquaglia <pilou.squaglia@laposte.net>
2021-09-16 00:12:03 +0200

    update

--- Atelier_4.py
+++ Atelier_4.py
@@ -5,6 +5,9 @@ Created on Tue Sep 14 09:21:53 2021
 @author: Pilou
 """
 
+
+import re 
+
 def full_name(str_arg:str) -> str:
     """
     prend en paramètre une chaine de caractère
@@ -30,7 +33,7 @@ avec la première lettre seulement en majuscule.
 print(full_name("bisgambiglia paul"))            
 
 
-def is_mail(str_arg:str) -> (int,int):
+def is_mail(mail:str) -> str:
     """
     prend en paramètre une chaine de
 caractère de type adresse mail ‘bisgambiglia@univ-corse.fr’ et qui renvoie un tuple composé
@@ -42,7 +45,52 @@ des codes suivants : (validité, code erreur)
 
     Returns
     -------
-    res : (int,int)
+    res : str
 
     """
-    
+    string_elements = mail.split("@")
+
+
+    if not("@" in mail):
+        return "[0, 2] le mail n’est pas valide, il manque l’@"
+        
+    #est-ce que le corps est valide? (composé uniquement d'alphanum,
+    # tirets et tirets bas, avec au moins 1 caractères et ne se finit pas par un point)
+    if re.match("^[_a-z0-9A-Z-]{2,}(\.[_a-zA-Z0-9-]+)*$", string_elements[0]) is None:
+            return "[0, 1] le mail n’est pas valide, le corp n’est pas valide"
+        
+
+    #"y a-t-il un point?"
+    second_part = string_elements[1].split(".")
+    if "." not in string_elements[1] or len(string_elements) != 2:
+        return "[0, 4] le mail n’est pas valide, il manque le ."
+
+    #le nom de domaine est-il "univ-corse" ?
+    if second_part[0] != "univ-corse":
+       
+            return "[0, 3] le mail n’est pas valide, le domaine n’est pas valide"
+
+    return "[1, 38] le mail est valide"
+
+
+
+#Exercice 2
+
+def mots_Nlettres(list_words: list, n: int):
+    corrects = []
+    for s in list_words:
+        if len(s) == n:
+            corrects.append(s)
+    return corrects
+
+
+def commence_par(mot: str, prefixe: str):
+    prefix_len = len(prefixe)
+    return mot[:prefix_len] == prefixe
+
+
+def finit_par(mot: str, suffixe: str):
+    suffixe_len = len(suffixe)
+    return mot[-suffixe_len:] == suffixe
+
+
--- Test_Atelier_4.py
+++ Test_Atelier_4.py
@@ -6,6 +6,7 @@ Created on Tue Sep 14 11:38:04 2021
 """
 
 from Atelier_4 import full_name
+from Atelier_4 import is_mail
 def test_full_name():
     
     nom='squaglia pierre-louis'
@@ -21,3 +22,18 @@ def test_full_name():
     return res
         
 print(test_full_name())
+
+def test_is_mail():
+    
+    
+    email_tests = [
+    "bisgambiglia_paul@univ-corse.fr",
+    "bisgambiglia_paulOuniv_corse.fr",
+    "bisgambiglia_paul@univ_corsePOINTfr",
+    "@univ_corse.fr",
+    "bisgambiglia_paul@univ_paris.fr"
+    ]
+    for email in email_tests:
+        print(is_mail(email))
+        
+test_is_mail()
