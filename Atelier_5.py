38a0e2e542bf4c8d90374d371aaaa0c3f817880b
PilouSquaglia <pilou.squaglia@laposte.net>
2021-09-20 08:44:23 +0200

    Update Atelier_5.py

--- Atelier_5.py
+++ Atelier_5.py
@@ -46,8 +46,24 @@ potentiellement triée et qui retourne la liste mélangée
     lst_res : list |liste passée en paramètre mélangée
 
     """
-    lst_res=[]
-    len_lst=len(list_to_mix)
-    for i in range(len_lst-1):
+    longueur=len(list_to_mix)
+    lst_res=[0]*longueur    #initialisation de lst_res
+    lst_new_indice=[] #liste des cases de lst_res qui ont deja ete remplies
+    for indice in range(longueur):#parcours les indices de la liste d'entree
+        new_indice=rd.randint(0, longueur-1)#recherche aleatoire d'un indice pour insertion dans la liste retournee
+        while new_indice in lst_new_indice:#on verifie que new_indice n'a pas deja ete utilise
+            new_indice=rd.randint(0, longueur-1)#sinon on en genere un autre
+        lst_res[new_indice]=list_to_mix[indice]#l'element est place dans une case de lst_res choisie au hasard
+        lst_new_indice.append(new_indice)#on ajoute new_indice a la liste pour ne pas le réutiliser
+    return lst_res
+
+
+
+
+lst_test=gen_list_random()
+print(lst_test)
+print(mix_list(lst_test))
+
+
         
         
